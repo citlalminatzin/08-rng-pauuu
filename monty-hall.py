b00d2eb53f088b3bdc172669d2c463e81b980578
Alba420p <albapaulina150@gmail.com>
2026-04-23 09:45:01 -0600

    Implement 'juego_gato' function for box selection game
    agrego funcion para juego del gatito

--- monty-hall.py
+++ monty-hall.py
@@ -1,7 +1,44 @@
 
 def create_game()->int:
     """Escoge la puerta ganadora"""
-    ...
+    import random
+
+def juego_gato():
+    print("Un gatito tiene 3 cajas: A, B, C")
+    print("Solo una tiene un pescado, las otras tienen pepinos\n")
+
+    cajas = ['A', 'B', 'C']
+    pescado = random.choice(cajas) 
+
+    eleccion = input("Elige una caja (A, B, C): ").upper()
+
+    if eleccion not in cajas:
+        print("Elección inválida")
+        return
+
+    posibles = [c for c in cajas if c != eleccion and c != pescado]
+    caja_abierta = random.choice(posibles)
+
+    print(f"\nSe abre la caja  {caja_abierta} y es un pepino.")
+
+
+    decision = input("¿Quieres cambiar de caja? (si/no): ").lower()
+
+    if decision == "si":
+        
+        restantes = [c for c in cajas if c != eleccion and c != caja_abierta]
+        eleccion = restantes[0]
+        print(f"Cambias a la caja {eleccion}")
+    else:
+        print(f"Te quedas con la caja {eleccion}")
+
+  
+    if eleccion == pescado:
+        print("¡El gatito encontró el pescado!")
+    else:
+        print(f"Había un pepino. El pescado estaba en la caja {pescado}")
+
+juego_gato()
 
 def play_change(n:int = 1000) -> float:
     """
